333cb101cc1829d7f76d1d4f54a6d04dbdec57fd
saparova2022 <112188786+saparova2022@users.noreply.github.com>
2022-11-06 21:45:37 -0600

    Add files via upload

---
+++ HumanPlayer.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.checkers_1;
+
+/**
+ *
+ * @author lalesaparova
+ */
+public class HumanPlayer extends Player {
+    
+    public boolean isHuman() {
+        return true;
+        
+    }
+    
+    public void updateGame(Game game) {}
+}
