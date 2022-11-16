af40101ea4338b9ab9c336554fe398d4989e81f2
DakotaCarpenterWIU <dm-carpenter@wiu.edu>
2022-11-16 03:15:38 -0600

    adding files and working jar file

--- CheckersGame_p2/src/com/mycompany/checkersgame/Player.java
+++ CheckersGame_p2/src/com/mycompany/checkersgame/Player.java
@@ -1,9 +1,4 @@
 package com.mycompany.checkersgame;
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 
 /**
  *
