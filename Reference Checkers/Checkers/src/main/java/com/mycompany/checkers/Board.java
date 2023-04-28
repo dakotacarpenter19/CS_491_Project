7f10a74d1b97a43e4be3708eb2b5588245960517
DakotaCarpenterWIU <dm-carpenter@wiu.edu>
2023-04-27 23:41:04 -0500

    Pieces move and move validation kinda works to an extent.

--- Reference Checkers/Checkers/src/main/java/com/mycompany/checkers/Board.java
+++ Reference Checkers/Checkers/src/main/java/com/mycompany/checkers/Board.java
@@ -1,177 +1,152 @@
-
 package com.mycompany.checkers;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.LinkedList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.Dimension;
-
-//import java.util.Arrays;
-//import com.mycompany.checkers.Game;
+import java.util.Arrays;
 
 /**
- *
  * @author Austin Winters, Dakota Carpenter
  */
-public class Board extends JFrame{
-           public static LinkedList<CPiece> cp = new LinkedList<>();
-           String[][] arr = new String[8][8];
-           
-           private int selectedRow = -1;
-           private int selectedCol = -1;
-           private int[][] board = {
-        		   {1, 0, 1, 0, 1, 0, 1, 0},
-        		   {0, 1, 0, 1, 0, 1, 0, 1},
-        		   {1, 0, 1, 0, 1, 0, 1, 0},
-        		   {0, 0, 0, 0, 0, 0, 0, 0},
-        		   {0, 0, 0, 0, 0, 0, 0, 0},
-        		   {0, 2, 0, 2, 0, 2, 0, 2},
-        		   {2, 0, 2, 0, 2, 0, 2, 0},
-        		   {0, 2, 0, 2, 0, 2, 0, 2},
-           };
-           
-    public Board(){
-        Game game = new Game();
-//          for(int i = 0; i < 8; i++ ){
-//              for(int j = 0; j<8; j++){
-//                  arr[j][i] = "O";
-//              }
-//          }
 
-          	//image array and images by Dakota Carpenter
-		   	Image[] img = new Image[4];
-           	img[0] = Toolkit.getDefaultToolkit().getImage("images/piece_black.png");
-           	img[1] = Toolkit.getDefaultToolkit().getImage("images/piece_red.png");
-           	img[2] = Toolkit.getDefaultToolkit().getImage("images/piece_red_king.png");
-           	img[3] = Toolkit.getDefaultToolkit().getImage("images/piece_black_king.png");
-          
-		   	JFrame frame = new JFrame();
-		   	frame.setBounds(10, 10, 526, 551);
-		   	frame.setTitle("Checkers");
-		   	frame.setResizable(false);
-            
-		   	// need to add an exit button and restart button to rebuild the board.
-			//may be able to use esc key to exit game and a popup window to declare winner and restart.
-            //JPanel for the displayable board and pieces by Austin Winters
-            JPanel panel = new JPanel(){
-                
-                @Override
-                public void paint(Graphics g){
-                	boolean isred = true;
-                	
-                	for (int a = 0; a < board.length; a++) {
-                		for (int b = 0; b < board[0].length; b++) {
-                			
-                			if ((a + b) % 2 == 0) {
-                				g.setColor(Color.WHITE);
-                			} else {
-                				g.setColor(Color.BLACK);
-                			}
-                			g.fillRect(b * 64, a * 64, 64, 64);
-                			
-                			if (board[a][b] == 1) {
-                				CPiece p = new CPiece(b, a, true, cp);
-                				g.setColor(Color.red);
-                				g.fillOval(b * 64 + 16, a * 64 + 16, 32, 32);
-                			} else if (board[a][b] == 2) {
-                				CPiece p = new CPiece(b, a, false, cp);
-                				g.setColor(Color.black);
-                				g.fillOval(b * 64 + 16, a * 64 + 16, 32, 32);
-                			} else if (board[a][b] == 0) {
-                				g.setColor(Color.red);
-                			}
-                			
-                			if (a == selectedRow && b == selectedCol) {
-                				g.setColor(Color.GREEN);
-                				g.drawRect(b * 64, a * 64, 63, 63);
-                			}
-                		}
-                	}
-                	
-                	int ind = 0;
-                	for(CPiece p : cp){
-                        if(p.isRed()){
-                            ind = 1;
-                        }
-                        else{
-                            ind = 0;
-                        }
-                    }
-                }
-            };
-            
-            frame.add(panel);
-            
-           frame.addMouseMotionListener(new MouseMotionListener() {
-            
-            	@Override
-            	public void mouseDragged(MouseEvent e) {}
-            	
-            	@Override
-            	public void mouseMoved(MouseEvent e) {}
-            });
+public class Board extends JFrame {
+	public static LinkedList<CPiece> cp = new LinkedList<>();
 
-            frame.addMouseListener(new MouseListener() {
-                int x;
-                int y;
-            	@Override
-            	public void mouseClicked(MouseEvent e) {
-            		int squareSize = 64;
-            		 int col, row;
-                       
-                             row = e.getY() / squareSize;
-                             col = e.getX() / squareSize;
-                        
-                       if(game.validMove(col, row)){      
-            		if (row < 0 || row >= 8 || col < 0 || col >= 8 ) {
-            			return;
-            		}
-            		if (selectedRow == -1 && board[row][col] != 0) {
-            			selectedRow = row;
-            			selectedCol = col;
-            		} else if (selectedRow != -1) {
-            			board[row][col] = board[selectedRow][selectedCol];
-            			board[selectedRow][selectedCol] = 0;
-            			selectedRow = -1;
-            			selectedCol = -1;
-            		} 
-                       }
-            		frame.repaint();
-//            		repaint();
-            		
-//            		System.out.println(Arrays.deepToString(board));
-            	}
-            	
-            	@Override
-            	public void mousePressed(MouseEvent e) {
-                        frame.repaint();
-            	}
-            	
-            	@Override
-            	public void mouseReleased(MouseEvent e) {}
-            	
-            	@Override
-            	public void mouseEntered(MouseEvent e) {}
-            	
-            	@Override
-            	public void mouseExited(MouseEvent e) {}
-            });
-            
-            frame.add(panel);
-            frame.setDefaultCloseOperation(3);
-            
-            frame.setVisible(true);
-     }
-    
-    // currently not used
-    public static CPiece getPiece(int x, int y) {
+	private int selectedRow = -1;
+	private int selectedCol = -1;
+	private static int[][] board = {
+			{1, 0, 1, 0, 1, 0, 1, 0},
+			{0, 1, 0, 1, 0, 1, 0, 1},
+			{1, 0, 1, 0, 1, 0, 1, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 2, 0, 2, 0, 2, 0, 2},
+			{2, 0, 2, 0, 2, 0, 2, 0},
+			{0, 2, 0, 2, 0, 2, 0, 2},
+	};
+
+	private int[] selectedPiece;
+
+	public Board() {
+		Game game = new Game();
+
+		JFrame frame = new JFrame();
+		frame.setBounds(0, 0, 528, 551);
+		frame.setTitle("Checkers");
+		frame.setResizable(false);
+
+		JPanel panel = new JPanel() {
+			@Override
+			public void paint(Graphics g) {
+//				boolean isred = true;
+
+				for (int a = 0; a < board.length; a++) {
+					for (int b = 0; b < board[a].length; b++) {
+						if ((a + b) % 2 == 0) {
+							g.setColor(Color.WHITE);
+						} else {
+							g.setColor(Color.BLACK);
+						}
+						g.fillRect(b * 64, a * 64, 64, 64);
+
+						if (board[a][b] == 1) {
+							CPiece p = new CPiece(b, a, true, cp);
+							g.setColor(Color.RED);
+							g.fillOval(b * 64 + 16, a * 64 + 16, 32, 32);
+						} else if (board[a][b] == 2) {
+							CPiece p = new CPiece(b, a, false, cp);
+							g.setColor(Color.BLACK);
+							g.fillOval(b * 64 + 16, a * 64 + 16, 32, 32);
+						} else if (board[a][b] == 0) {
+							g.setColor(Color.RED); // not sure why this is here
+						}
+
+						if (a == selectedRow && b == selectedCol) {
+							g.setColor(Color.GREEN);
+							g.drawRect(b * 64, a * 64, 63, 63);
+						}
+					}
+				}
+			}
+		};
+
+		frame.add(panel);
+		frame.addMouseMotionListener(new MouseMotionListener() {
+			@Override
+			public void mouseDragged(MouseEvent e) {}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {}
+		});
+
+		frame.addMouseListener(new MouseListener() {
+			int x;
+			int y;
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+
+
+				int squareSize = 64;
+				int col, row;
+
+				row = e.getY() / squareSize;
+				col = e.getX() / squareSize;
+
+				if (game.getSelectedPiece() == null) {
+					game.setSelectedPiece(row, col);
+				} else {
+					game.movePiece(row, col);
+				}
+
+//				if (game.isValidMove(row, col, selectedRow, selectedCol)) {
+//					if (row < 0 || row > 7 || col < 0 || col > 7) {
+//						return;
+//					}
+//					if (selectedRow == -1 && board[row][col] != 0) {
+//						selectedRow = row;
+//						selectedCol = col;
+//					} else if (selectedRow != -1) {
+//						board[row][col] = board[selectedRow][selectedCol];
+//						board[selectedRow][selectedCol] = 0;
+//						selectedRow = -1;
+//						selectedCol = -1;
+//					}
+//				}
+
+				frame.repaint();
+
+				System.out.println(Arrays.deepToString(board));
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				frame.repaint();
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+
+			@Override
+			public void mouseExited(MouseEvent e) {}
+		});
+
+		frame.add(panel);
+		frame.setDefaultCloseOperation(3);
+		frame.setVisible(true);
+
+	}
+
+	public static CPiece getPiece(int x, int y) {
 		int xp = x / 64;
 		int yp = y / 64;
 
@@ -183,14 +158,7 @@ public class Board extends JFrame{
 		return null;
 	}
 
-//	private JButton exit() {
-//		JButton endGame = new JButton();
-//		endGame.setPreferredSize(new Dimension(91, 25));
-//		endGame.setMaximumSize(new Dimension(91, 25));
-//		endGame.setAlignmentX(10);
-//		endGame.setAlignmentY(-10);
-//		endGame.setVisible(true);
-//		return endGame;
-//	}
-
+	public static int[][] getBoard() {
+		return board;
+	}
 }
--- Reference Checkers/Checkers/src/main/java/com/mycompany/checkers/Game.java
+++ Reference Checkers/Checkers/src/main/java/com/mycompany/checkers/Game.java
@@ -3,141 +3,233 @@ package com.mycompany.checkers;
 
 /**
  *
- * @author wincr
+ * @author Austin Winters, Dakota Carpenter
  */
 public class Game {
     private boolean validate;
     private boolean isFull;
     private boolean pdestroy;
+
+    private int currentPlayer;
+
+    private int[][] board;
+
+    private int[] selectedPiece;
    
     public Game(){
         this.validate = false;
         this.isFull = false;
         this.pdestroy = false;
+
+        board = Board.getBoard();
+
     }
-    public boolean validMove(int x, int y){
-        
-        if(y==8){
-            y--;
-        }
-        if(x==8){
-            x--;
-        }
-        if(x ==0 && y == 0 ){
-           this.validate = true;
-        }
-        else if(x == 0 && y == 2 ){
-        
-           this.validate = true;
-        }
-        else if(x == 0 && y == 4){
-            this.validate = true;
-        }
-        else if(x == 0 && y == 6){
-            this.validate = true;
-        }
-        else if(x==1 && y==1){
-            this.validate = true;            
-        }
-        else if(x==1 && y==3 ){
-            this.validate = true;
-        }
-        else if(x==1 && y==5){
-            this.validate = true;
-        }
-        else if(x==1 && y==7){
-            this.validate = true;
-        }
-        else if(x == 2 && y == 0){
-            this.validate = true;
-        }
-        else if(x==2 && y == 2){
-            this.validate = true;
-        }
-        else if(x==2 && y==4){
-            this.validate = true;
-        }
-        else if(x==2 && y==6){
-            this.validate = true;
-        }
-        else if(x==3 && y==1){
-            this.validate = true;            
-        }
-        else if(x==3 && y==3 ){
-            this.validate = true;
-        }
-        else if(x==3 && y==5){
-            this.validate = true;
-        }
-        else if(x==3 && y==7){
-            this.validate = true;
-        }
-        else if(x == 4 && y == 0){
-            this.validate = true;
-        }
-        else if(x==4 && y == 2){
-            this.validate = true;
-        }
-        else if(x==4 && y==4){
-            this.validate = true;
-        }
-        else if(x==4 && y==6){
-            this.validate = true;
-        }
-        else if(x==5 && y==1){
-            this.validate = true;            
-        }
-        else if(x==5 && y==3 ){
-            this.validate = true;
-        }
-        else if(x==5 && y==5){
-            this.validate = true;
-        }
-        else if(x==5 && y==7){
-            this.validate = true;
-        }
-        else if(x == 6 && y == 0){
-            this.validate = true;
-        }
-        else if(x==6 && y == 2){
-            this.validate = true;
-        }
-        else if(x==6 && y==4){
-            this.validate = true;
-        }
-        else if(x==6 && y==6){
-            this.validate = true;
-        }
-        else if(x==7 && y==1){
-            this.validate = true;            
-        }
-        else if(x==7 && y==3 ){
-            this.validate = true;
-        }
-        else if(x==7 && y==5){
-            this.validate = true;
-        }
-        else if(x==7 && y==7){
-            this.validate = true;
-        }
-        else{
-            this.validate = false;
-        }
-        
-        return this.validate;
-    }
-    public boolean isFull(int x1, int y1, int x2, int y2){
-        if(x1 == x2 && y1 == y2){
-            this.isFull = true;
-        
-        }
-        return this.isFull;
-    }
-    public boolean destroyPiece(int x1, int y1, int x2, int y2){
-            
-            if((x1 == 3 && y1 == 5) && (x2 == 1 && y2 == 3)){
-                   pdestroy = true; 
+
+    public int[][] constructBoard() {
+        int[][] newBoard = new int[8][8];
+        System.out.println("" + newBoard.length + " " + newBoard[0].length);
+        for (int i = 0; i < newBoard.length; i++) {
+            for (int j = 0; i < newBoard[i].length; j++) {
+                if ((i + j) % 2 == 1 && i < 3) {
+                    newBoard[i][j] = 1;
+                } else if ((i + j) % 2 == 1 && i > 4) {
+                    newBoard[i][j] = 2;
+                } else {
+                    newBoard[i][j] = 0;
+                }
             }
-         return pdestroy;   
+        }
+        return newBoard;
     }
+
+    public int[] getSelectedPiece() {
+        return selectedPiece;
+    }
+
+    public void setSelectedPiece(int row, int col) {
+        selectedPiece = new int[]{row, col};
+    }
+
+    public void movePiece(int row, int col) {
+        if (isValidMove(selectedPiece[0], selectedPiece[1], row, col)) {
+            board[row][col] = board[selectedPiece[0]][selectedPiece[1]];
+            board[selectedPiece[0]][selectedPiece[1]] = 0;
+        }
+
+        selectedPiece = null;
+
+    }
+
+    public boolean isValidMove(int startRow, int startCol, int endRow, int endCol) {
+        System.out.println("startRow: " + startRow + " startCol: " + startCol + " endRow: " + endRow + " endCol: " + endCol);
+
+        // Checks if the end position is within the board
+        if (endRow < 0 || endRow > 7 || endCol < 0 || endCol > 7) {
+            System.out.println("endRow = " + endRow + " and endCol = " + endCol);
+            System.out.println("Invalid move: end position is not within the board");
+            return false;
+        }
+        // Checks if the end position is empty
+        if (board[endRow][endCol] != 0) {
+            System.out.println("Invalid move: end position is not empty");
+            return false;
+        }
+        // Checks if the move is diagonal
+        if (Math.abs(endRow - startRow) != Math.abs(endCol - startCol)) {
+            System.out.println("Invalid move: move is not diagonal");
+            return false;
+        }
+        // Checks if the piece is moving in the correct direction
+        if (board[startRow][startCol] == 2 && endRow > startRow) {
+            System.out.println("Invalid move: red piece not moving in the correct direction");
+            return false;
+        }
+        if (board[startRow][startCol] == 1 && endRow < startRow) {
+            System.out.println("Invalid move: black piece not moving in the correct direction");
+            return false;
+        }
+        // Checks if the move is regular or jump
+        if (Math.abs(endRow - startRow) == 1) {
+            return true; // regular move
+        }
+
+        int jumpedRow = (startRow + endRow) / 2;
+        int jumpedCol = (startCol + endCol) / 2;
+        if (board[jumpedRow][jumpedCol] == 0) {
+            System.out.println("Invalid move: no jumpable piece");
+            return false; // no jumpable piece
+        }
+        if (board[jumpedRow][jumpedCol] == currentPlayer || board[jumpedRow][jumpedCol] == currentPlayer + 2) {
+            System.out.println("Invalid move: cannot jump over your own piece");
+            return false; // cannot jump over your own pieces
+        }
+        return true; // actually jump
+    }
+
+
+//    public boolean validMove(int x, int y){
+//
+//        if(y==8){
+//            y--;
+//        }
+//        if(x==8){
+//            x--;
+//        }
+//        if(x ==0 && y == 0 ){
+//           this.validate = true;
+//        }
+//        else if(x == 0 && y == 2 ){
+//
+//           this.validate = true;
+//        }
+//        else if(x == 0 && y == 4){
+//            this.validate = true;
+//        }
+//        else if(x == 0 && y == 6){
+//            this.validate = true;
+//        }
+//        else if(x==1 && y==1){
+//            this.validate = true;
+//        }
+//        else if(x==1 && y==3 ){
+//            this.validate = true;
+//        }
+//        else if(x==1 && y==5){
+//            this.validate = true;
+//        }
+//        else if(x==1 && y==7){
+//            this.validate = true;
+//        }
+//        else if(x == 2 && y == 0){
+//            this.validate = true;
+//        }
+//        else if(x==2 && y == 2){
+//            this.validate = true;
+//        }
+//        else if(x==2 && y==4){
+//            this.validate = true;
+//        }
+//        else if(x==2 && y==6){
+//            this.validate = true;
+//        }
+//        else if(x==3 && y==1){
+//            this.validate = true;
+//        }
+//        else if(x==3 && y==3 ){
+//            this.validate = true;
+//        }
+//        else if(x==3 && y==5){
+//            this.validate = true;
+//        }
+//        else if(x==3 && y==7){
+//            this.validate = true;
+//        }
+//        else if(x == 4 && y == 0){
+//            this.validate = true;
+//        }
+//        else if(x==4 && y == 2){
+//            this.validate = true;
+//        }
+//        else if(x==4 && y==4){
+//            this.validate = true;
+//        }
+//        else if(x==4 && y==6){
+//            this.validate = true;
+//        }
+//        else if(x==5 && y==1){
+//            this.validate = true;
+//        }
+//        else if(x==5 && y==3 ){
+//            this.validate = true;
+//        }
+//        else if(x==5 && y==5){
+//            this.validate = true;
+//        }
+//        else if(x==5 && y==7){
+//            this.validate = true;
+//        }
+//        else if(x == 6 && y == 0){
+//            this.validate = true;
+//        }
+//        else if(x==6 && y == 2){
+//            this.validate = true;
+//        }
+//        else if(x==6 && y==4){
+//            this.validate = true;
+//        }
+//        else if(x==6 && y==6){
+//            this.validate = true;
+//        }
+//        else if(x==7 && y==1){
+//            this.validate = true;
+//        }
+//        else if(x==7 && y==3 ){
+//            this.validate = true;
+//        }
+//        else if(x==7 && y==5){
+//            this.validate = true;
+//        }
+//        else if(x==7 && y==7){
+//            this.validate = true;
+//        }
+//        else{
+//            this.validate = false;
+//        }
+//
+//        return this.validate;
+//    }
+//    public boolean isFull(int x1, int y1, int x2, int y2){
+//        if(x1 == x2 && y1 == y2){
+//            this.isFull = true;
+//
+//        }
+//        return this.isFull;
+//    }
+//    public boolean destroyPiece(int x1, int y1, int x2, int y2){
+//
+//            if((x1 == 3 && y1 == 5) && (x2 == 1 && y2 == 3)){
+//                   pdestroy = true;
+//            }
+//         return pdestroy;
+//    }
 }
